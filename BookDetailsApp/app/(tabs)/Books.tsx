291c8a6358fc80820de3d2bf7c0ad5d921110ca2
KatarinaGovedarica <145567220+ka7arina@users.noreply.github.com>
2024-11-01 13:05:08 +0100

    created filter bar for books page

--- BookDetailsApp/app/(tabs)/Books.tsx
+++ BookDetailsApp/app/(tabs)/Books.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { StyleSheet, View, ImageBackground } from 'react-native';
 import { PaperProvider, Text } from 'react-native-paper';
 import styles from '../../constants/Styles';
+import FilterBar from '../../components/FilterBar'
+import BookCard from "@/components/BookCard";
 
 
 export default function BookScreen() {
   return (
     <PaperProvider>
       <View style={pageStyles.container}>
-        <Text style={[styles.welcomeText, { color: styles.cardTitle.color }]}>Welcome to the Book Details App</Text>
+        <FilterBar/>
+        <BookCard/>
+        <BookCard/>
       </View>
     </PaperProvider>
   );
