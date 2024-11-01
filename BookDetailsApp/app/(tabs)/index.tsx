d03a981ce9fb428e809e10528774dd34cd9d4333
KatarinaGovedarica <145567220+ka7arina@users.noreply.github.com>
2024-11-01 10:47:23 +0100

    added BookCardComponent into index.tsx (testing purposes)

--- BookDetailsApp/app/(tabs)/index.tsx
+++ BookDetailsApp/app/(tabs)/index.tsx
@@ -2,12 +2,14 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import BookCard from '@/components/BookCard';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to BookDetailschecker </Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <BookCard/>
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
