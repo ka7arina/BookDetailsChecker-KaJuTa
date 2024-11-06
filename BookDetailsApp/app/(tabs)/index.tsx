eb9dce1836176aabe14135076882be607bca975b
juliana <145568533+jvl1ana@users.noreply.github.com>
2024-11-06 14:45:30 +0100

    Phuc: resolved issue with Index and index

--- BookDetailsApp/app/(tabs)/index.tsx
+++ BookDetailsApp/app/(tabs)/index.tsx
@@ -1,8 +1,48 @@
-// app/(tabs)/index.tsx
+import React from 'react';
+import {StyleSheet, View, ImageBackground, Text} from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import '../../assets/images/background-image.png';
+import {Card} from 'react-native-paper';
 
-import { Redirect } from "expo-router";
+export default function HomeScreen() {
+  return (
+      <PaperProvider>
+        <ImageBackground
+            source={require('../../assets/images/background-image.png')}
+            style={styles.background}
+        >
+          <View style={styles.container}>
+              <Card>
+                  <Card.Content>
+                      <Text style={styles.title}>Welcome to the
+                          Book Crux Library </Text>
+                  </Card.Content>
+              </Card>
+          </View>
+        </ImageBackground>
+      </PaperProvider>
+  );
+}
 
-const Index = () => {
-    return <Redirect href="/Home" />;
-};
-export default Index;
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  scrollViewContent: {
+    paddingTop: 60,
+  },
+  cardContainer: {
+    width: '100%',
+    alignItems: 'center',
+  },
+});
